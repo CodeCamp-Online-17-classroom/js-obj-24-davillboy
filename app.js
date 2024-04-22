aeea268036c87fe3ddbf44a3a1f73aada80ec51b
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 19:23:11 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // write code here
+class Sale {
+  constructor(name) {
+    this.name = name;
+  }
+
+  calcPrice(amount, price, discount) {
+    let total = amount * price;
+    let discountedPrice = total - (total * discount);
+    return discountedPrice;
+  }
+}
+
+class Beverage extends Sale {
+  constructor(name, amount, price, discount) {
+    super(name);
+    this.amount = amount;
+    this.price = price;
+    this.discount = discount;
+  }
+}
+
+// สร้าง object beverage จาก Beverage
+let beverage = new Beverage('Pepsi', 3, 19, 0.1);
+
+// ให้ beverage เรียก method calcPrice() และแสดงผล
+let totalPrice = beverage.calcPrice(beverage.amount, beverage.price, beverage.discount);
+console.log('Total Price after discount:', totalPrice);
